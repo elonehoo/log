e77dc0a16821714e52d6781303daef31307bbabe
xiaoxunyao <huchengyea@163.com>
2022-05-07 19:57:31 +0800

    fix: update

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,38 @@
-import {convertName} from '@titem/convert/src/index'
+import {settings} from './type/setting'
+import typeToPrintf from './util/typeToPrintf'
 
-export default convertName
+let CURRENT_LOG_LEVEL = 2 // error level by default
+
+const tryToLog =
+    (level: number, scope = "") =>
+        (params: any[]) => {
+            if (CURRENT_LOG_LEVEL >= level) {
+                if (settings[level]) {
+                    const [tag, colors, log_fn] = settings[level];
+                    log_fn.apply(
+                        null,
+                        [
+                            `${scope} %c #%s %c ${params.map(typeToPrintf).join(" ")}`,
+                            `background-color:${colors[0] || ""};color:white;`,
+                            tag,
+                            colors[1] ? `color:${colors[1]};` : ""
+                        ].concat(params)
+                    );
+                }
+            }
+        };
+
+
+const scope = (name?: string) => ({
+    trace: (...params: unknown[]) => tryToLog(6, name)(params),
+    debug: (...params: unknown[]) => tryToLog(5, name)(params),
+    warn: (...params: unknown[]) => tryToLog(4, name)(params),
+    error: (...params: unknown[]) => tryToLog(3, name)(params),
+    info: (...params: unknown[]) => tryToLog(2, name)(params),
+    fatal: (...params: unknown[]) => tryToLog(1, name)(params)
+})
+
+export default Object.assign(scope(), {
+  scope,
+  setLevel: (level: number) => (CURRENT_LOG_LEVEL = level)
+})
