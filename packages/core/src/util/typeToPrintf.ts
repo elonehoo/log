0bc0f3a1485e37ba8ba38a1663dc327bdc214271
xiaoxunyao <huchengyea@163.com>
2022-05-07 19:57:39 +0800

    chore: init `log`

---
+++ packages/core/src/util/typeToPrintf.ts
@@ -0,0 +1,14 @@
+function typeToPrintf(item:any){
+  switch (typeof item) {
+    case "string":
+        return "%s"
+    case "boolean":
+        return "%o"
+    case "number":
+        return (0 ^ item) === item ? "%i" : "%f"
+    default:
+        return "%O"
+}
+}
+
+export default typeToPrintf
